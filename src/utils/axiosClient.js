312519cb265bbf24ac990aa791e772f231942532
Akash Kumar <akashkumar975887@gmail.com>
2025-12-01 03:06:23 +0530

    second commit

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",   // Apne backend ka base URL
+  baseURL: "https://llms-chatbot.onrender.com",   // Apne backend ka base URL
 
 });
 
